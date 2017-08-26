51d6a42ebdae7a832c3a0994ed51008131742328
unknown <pchouh@AGI-LT-M052.netdecisions.local>
2017-08-26 14:21:23 +0530

    Initial Commit

---
+++ SSHNET/Program.cs
@@ -0,0 +1,66 @@
+﻿using Renci.SshNet;
+using Renci.SshNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSHNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using SSH.NET");
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            watch.Start();
+
+            //Connecting to WinSCP
+            var username = "AzurePrad";
+            var password = "AzureIndia789!!";
+            var host = "52.187.3.217";
+            int port = 22;
+            string hostKey = "ssh-rsa 2048 59:22:9e:5b:eb:c3:6c:c0:2a:01:7f:cc:b3:1e:68:aa";
+
+            //Connecting to RebexTinySftpServer
+            //var username = "tester";
+            //var password = "password";
+            //var host = "52.187.3.217";
+            //int port = 22;
+
+            var connectInfo = new ConnectionInfo(host, port, username, new PasswordAuthenticationMethod(username, password));
+
+            connectInfo.Timeout = TimeSpan.FromSeconds(100);
+
+            using (var client = new SftpClient(connectInfo))
+            {
+                //client.HostKeyReceived += delegate (object sender, HostKeyEventArgs e)
+                //{
+                //    //if (e.FingerPrint.SequenceEqual(ConvertFingerprintToByteArray("1d:c1:5a:71:c4:8e:a3:ff:01:0a:3b:46:17:6f:e1:52")))
+                //    //    e.CanTrust = true;
+                //    //else
+                //    //    e.CanTrust = false;
+                //    e.CanTrust = true;
+                //};
+
+                client.Connect();
+                Console.WriteLine($"IsConnected: {client.IsConnected}");
+                client.Disconnect();
+                Console.WriteLine($"IsConnected: {client.IsConnected}");
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"Time taken for connect: {watch.Elapsed.Seconds}s");
+        }
+
+        public static byte[] ConvertFingerprintToByteArray(String fingerprint)
+        {
+            return fingerprint.Split(':').Select(s => Convert.ToByte(s, 16)).ToArray();
+        }
+    }
+}
---
+++ WinSCPClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinSCP;
+
+namespace WinSCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using WinSCP Client");
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            watch.Start();
+
+            //Connecting to WinSCP
+            var username = "AzurePrad";
+            var password = "AzureIndia789!!";
+            var host = "52.187.3.217";
+            int port = 22;
+            string hostKey = "ssh-rsa 2048 59:22:9e:5b:eb:c3:6c:c0:2a:01:7f:cc:b3:1e:68:aa";
+
+            //Connecting to RebexTinySftpServer
+            //var username = "tester";
+            //var password = "password";
+            //var host = "52.187.3.217";
+            //int port = 22;
+            //string hostKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQ38sCI2XLgD74f3yNLKt17uTDuA0D1RJEmE19G9UBLoJvRRwTRk8lO/pEQ1uLW69u0mC34Su+XWtt2B1TFRTFZR0sFxD5Iz/61Zw7wTZPCaufMSrWBryd5TQ+sW5l6CSyvp00WUOyRIgY+OMTS4DcFLKt0iyKdScuhCSBFcewGqceFdJPdUj62rmgLCrPfr3vZO/rQGo0ipy4mexviCljE1yOvfFCC/4e4+Fap/MLlo8ogl3aLobDisBZkgZo9VIACMyjwcpfmdcnVEvwkpLta/2QRA6eEJcDNnWl32RzHZ1CbQkMH+63EDQlbVUCCEi3Tx6bg6MJ/GLkFyPjLkdP";
+
+
+            // Setup session options
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Sftp,
+                HostName = host,
+                PortNumber = port,
+                UserName = username,
+                Password = password,
+                SshHostKeyFingerprint = hostKey
+            };
+
+            using (Session session = new Session())
+            {
+                // Connect
+                session.Open(sessionOptions);
+
+                Console.WriteLine($"IsConnected: {session.Opened}");
+
+                RemoteDirectoryInfo directory = session.ListDirectory("/C:/Agdio");
+
+                foreach (RemoteFileInfo fileInfo in directory.Files)
+                {
+                    Console.WriteLine("{0} with size {1}, permissions {2} and last modification at {3}",
+                        fileInfo.Name, fileInfo.Length, fileInfo.FilePermissions, fileInfo.LastWriteTime);
+                }
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"Time taken for connect: {watch.Elapsed.Seconds}s");
+        }
+    }
+}
